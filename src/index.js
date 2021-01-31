7f6b5696856f7b58b1c721b22b31405defb18027
Andrew Stanton <up683693@myport.ac.uk>
2021-01-31 17:44:31 +0000

    Detect winning conditions
    Tweaks
    - Used tertiary operators and interpolation for status
    - Replaced the `for` loop with `for ... of`

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ class Board extends React.Component {
 
 	handleClick(i) {
 		const squares = this.state.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
 		squares[i] = this.state.xIsNext ? 'X' : 'O';
 		this.setState({
 			squares: squares,
@@ -40,7 +43,10 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+		const winner = calculateWinner(this.state.squares);
+		const status = winner
+			? `Winner: ${winner}`
+			: `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
 		return (
 			<div>
@@ -81,6 +87,29 @@ class Game extends React.Component {
 	}
 }
 
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (const line of lines) {
+		const [a, b, c] = line;
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
+}
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'));
